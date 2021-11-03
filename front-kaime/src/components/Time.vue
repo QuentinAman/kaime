58e1d0909d98fa183b97c82a734ab2bd134bbcf7
Maxime Di Natale <maxime@oddiris.com>
2021-11-03 11:02:23 +0100

    Time Story

---
+++ front-kaime/src/components/Time.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  hours: Number,
+  minutes: Number,
+});
+
+const now = new Date();
+now.setHours(props.hours, props.minutes);
+</script>
+
+<template>
+  <div class="container">
+    <p>{{ now.toLocaleTimeString().replace(/:[^:]+$/, "") }}</p>
+    <div class="bar" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+p {
+  position: absolute;
+  font-weight: 900;
+}
+
+.bar {
+  flex: 1;
+  height: 1px;
+  background-color: currentColor;
+  margin-left: 6ch;
+}
+</style>
