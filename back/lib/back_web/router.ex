f2ff7a3cb37ad8f559678fbe7ebb93aac4f4de50
QuentinAman <quentin.aman@outlook.com>
2021-11-08 15:45:46 +0100

    Docker

--- back/lib/back_web/router.ex
+++ back/lib/back_web/router.ex
@@ -13,33 +13,48 @@ defmodule BackWeb.Router do
   scope "/api", BackWeb do
     pipe_through :api
 
+    # GLOBAL ROUTES
     post "/sign_up", UserController, :signup
     post "/sign_in", UserController, :signin
 
+    # USER ROUTES
     scope "/users" do
       pipe_through :auth
-      get "/", UserController, :index
       get "/self", UserController, :get_self
+      # TODO
+      patch "/self", UserController, :update
+      delete "/self", UserController, :delete
 
+      # WORKINGTIME ROUTES
       get "/workingtimes", WorkingtimeController, :get_mines
       post "/workingtime", WorkingtimeController, :create
       delete "/workingtime", WorkingtimeController, :delete
       patch "/workingtime", WorkingtimeController, :update
 
+      # CLOCK ROUTES
       get "/clock", ClockController, :show
       patch "/clock", ClockController, :update
     end
 
+    # ADMIN ROUTES
+    # TODO
     scope "/admin" do
       pipe_through :auth
-      pipe_through :admin
+      # pipe_through :admin
+
+      delete "/user", UserController, :delete_user
+      get "/", UserController, :index
     end
 
+    # MANAGER ROUTES
+    # TODO
     scope "/manager" do
       pipe_through :auth
-      pipe_through :manager
+      # pipe_through :manager
 
       get "/team", TeamController, :show
+      post "/team", TeamController, :add_to_team
+      patch "/team", TeamController, :remove_from_team
     end
   end
 end
