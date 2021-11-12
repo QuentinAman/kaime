1faf8978383421b90ab53c6e50443eaa4fd052de
QuentinAman <quentin.aman@outlook.com>
2021-11-12 10:22:15 +0100

    Get teams from admin

--- back/lib/back_web/router.ex
+++ back/lib/back_web/router.ex
@@ -2,65 +2,68 @@ defmodule BackWeb.Router do
   use BackWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
-    plug CORSPlug
+    plug(:accepts, ["json"])
+    plug(CORSPlug)
   end
 
   pipeline :auth do
-    plug Back.AuthPlug
+    plug(Back.AuthPlug)
   end
 
   pipeline :admin do
-    plug Back.AdminPlug
+    plug(Back.AdminPlug)
   end
 
   pipeline :manager do
-    plug Back.ManagerPlug
+    plug(Back.ManagerPlug)
   end
 
   scope "/api", BackWeb do
-    pipe_through :api
+    pipe_through(:api)
 
     # GLOBAL ROUTES
-    post "/sign_up", UserController, :signup
-    post "/sign_in", UserController, :signin
+    post("/sign_up", UserController, :signup)
+    post("/sign_in", UserController, :signin)
 
     # USER ROUTES
     scope "/self" do
-      pipe_through :auth
-      get "/", UserController, :get_self
-      patch "/", UserController, :update
-      delete "/", UserController, :delete
-      patch "/password", UserController, :update_password
+      pipe_through(:auth)
+      get("/", UserController, :get_self)
+      patch("/", UserController, :update)
+      delete("/", UserController, :delete)
+      patch("/password", UserController, :update_password)
 
       # WORKINGTIME ROUTES
-      get "/workingtimes", WorkingtimeController, :get_mines
-      post "/workingtimes", WorkingtimeController, :create
-      delete "/workingtimes/:id", WorkingtimeController, :delete
-      patch "/workingtimes/:id", WorkingtimeController, :update
+      get("/workingtimes", WorkingtimeController, :get_mines)
+      post("/workingtimes", WorkingtimeController, :create)
+      delete("/workingtimes/:id", WorkingtimeController, :delete)
+      patch("/workingtimes/:id", WorkingtimeController, :update)
 
       # CLOCK ROUTES
-      get "/clock", ClockController, :show
-      patch "/clock", ClockController, :update
+      get("/clock", ClockController, :show)
+      patch("/clock", ClockController, :update)
     end
 
     # ADMIN ROUTES
     scope "/admin" do
-      pipe_through :admin
+      pipe_through(:admin)
 
-      delete "/user", UserController, :delete_user
-      get "/", UserController, :index
-      patch "/update_role", UserController, :update_role
+      delete("/user", UserController, :delete_user)
+      get("/", UserController, :index)
+      patch("/update_role", UserController, :update_role)
+      get("/teams", TeamController, :index)
+      # post("/create_team", TeamController, :create_team) #
+      # patch("create_manager", UserManager, :create_manager) #
+      # delete("/delete_team", TeamController, :delete_team) #
     end
 
     # MANAGER ROUTES
-    # TODO
     scope "/manager" do
-      pipe_through :manager
+      pipe_through(:manager)
 
-      get "/team", TeamController, :show
-      post "/team", UserController, :add_to_team
-      patch "/team", UserController, :remove_from_team
+      # get("/team", TeamController, :show) #
+      # post("/team", UserController, :add_to_team) #
+      # patch("/team", UserController, :remove_from_team) #
     end
   end
 end
